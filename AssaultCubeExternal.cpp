48886a3e644db5258d8b631ae3d656c72b1a31d7
Enething <enethingofficial@gmail.com>
2023-11-21 00:51:59 +0100

    Initial Commit

---
+++ AssaultCubeExternal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    //  Get procid
+
+    // get handle to right process
+
+    // Resolve base addres to pointer chain
+
+    // Resolve right pointer
+
+     // Read and Write to pointer value
+
+    return 0;
+}
