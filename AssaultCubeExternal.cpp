32ca15f18b1950a1678eb064f5076f7d2d4b8bb7
Enething <115583828+theyluvEnething@users.noreply.github.com>
2023-12-19 19:11:29 +0100

    FInished WriteProcess

--- AssaultCubeExternal.cpp
+++ AssaultCubeExternal.cpp
@@ -3,48 +3,65 @@
 #include "proc.h"
 #include <Windows.h>
 #include <vector>
+#include <TlHelp32.h>
+#include <stdlib.h>
+#include <tchar.h>
+
 
 int main()
 {
-    
-
     // testing
     // *[ac_client.exe+17E0A8] + 140 == ammo value
 
 
     std::cout << "Assault Cube External!\n";
-    //  Get procid
-    DWORD procId = GetProcId(L"ac_client.exe");
-    std::cout << procId << std::endl;
-       
-    // get module base adress
-    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
 
-    // get handle to right process
-    HANDLE hProcess = 0;
-    hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-    
-    // Resolve base addres to pointer chain
-    std::cout << "ModuleBase: " << moduleBase << std::endl;
-    uintptr_t dynamicPtrBaseAddress = moduleBase + 0x17E0A8;
-    std::cout << "DynamicBaseAddress = " << "0x" << std::hex << dynamicPtrBaseAddress << std::endl;
+    HWND hwnd_AC = FindWindowA(NULL, "AssaultCube"); // get handle to window
 
-    // Resolve right pointer
-    std::vector<unsigned int> ammoOffset = {0x140};
-    uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddress, ammoOffset);
-    std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
+    if (hwnd_AC != FALSE);
+
+    DWORD pid = NULL;
+    GetWindowThreadProcessId(hwnd_AC, &pid);
+    HANDLE pHandle = NULL;
+    pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (pHandle == INVALID_HANDLE_VALUE || pHandle == NULL);
+
+    wchar_t gameModule[] = L"ac_client.exe";
+    DWORD gameBaseAddress = GetModuleBaseAddress(gameModule, pid);
+
+    DWORD ammoAddr = 0x195404;
+    std::vector<DWORD> ammoOffsets{ 0x140 };
+    DWORD ammoPtrAdress = GetPointerAddress(hwnd_AC, gameBaseAddress, ammoAddr, ammoOffsets);
+
+    //write memory
+    while (true) {
+        int ammo = 1337;
+        WriteProcessMemory(pHandle, (LPVOID*)(ammoPtrAdress), &ammo, 4, 0);
+    }
+
+    ////  Get procid
+    //DWORD procId = GetProcId(L"ac_client.exe");
+    //std::cout << procId << std::endl;
+    //   
+    //// get module base adress
+    //uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
+
+    //// get handle to right process
+    //HANDLE hProcess = 0;
+    //hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+    //
+    //// Resolve base addres to pointer chain
+    //std::cout << "ModuleBase: " << moduleBase << " | 0x" << std::hex << moduleBase << std::endl;
+
+    //// Resolve right pointer
+    //DWORD playerAddress = 0x195404;
+    //std::vector<unsigned int> ammoOffsets = { 0xEC };
+    //uintptr_t healthAddr = FindDMAAddy(hProcess, moduleBase, ammoOffsets);
+    //std::cout << "healthAddr = " << healthAddr << " | 0x" << std::hex << healthAddr << std::endl;
 
 
-     // Read and Write to pointer value
-    int ammoValue = 0;
-    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-    std::cout << "Current Ammo = " << std::dec << ammoValue << std::endl;
 
-    int newAmmo = 1337;
-    WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
 
-    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-    std::cout << "New Ammo = " << std::dec << ammoValue << std::endl;
 
     getchar();
 
--- proc.cpp
+++ proc.cpp
@@ -1,6 +1,7 @@
 #include "proc.h"
 #include "stdafx.h"
 #include <wchar.h>
+#include <vector>
 
 DWORD GetProcId(const wchar_t* procName) {
 	DWORD procId = 0;
@@ -24,33 +25,74 @@ DWORD GetProcId(const wchar_t* procName) {
 	return procId;
 }
 
-uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
-	uintptr_t modBaseAddress = 0;
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
-	if (hSnap != INVALID_HANDLE_VALUE) {
-		
-		MODULEENTRY32 modEntry;
-		modEntry.dwSize = sizeof(modEntry);
-		
-		if (Module32First(hSnap, &modEntry)) {
-			do {
-				if (!_wcsicmp(modEntry.szModule, modName)) {
-					modBaseAddress = (uintptr_t)modEntry.modBaseAddr;
-					break;
-				}
-			} while (Module32Next(hSnap, &modEntry));
-		}
+//uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
+//	uintptr_t modBaseAddress = 0;
+//	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+//	if (hSnap != INVALID_HANDLE_VALUE) {
+//		
+//		MODULEENTRY32 modEntry;
+//		modEntry.dwSize = sizeof(modEntry);
+//		
+//		if (Module32First(hSnap, &modEntry)) {
+//			do {
+//				if (!_wcsicmp(modEntry.szModule, modName)) {
+//					modBaseAddress = (uintptr_t)modEntry.modBaseAddr;
+//					break;
+//				}
+//			} while (Module32Next(hSnap, &modEntry));
+//		}
+//	}
+//	CloseHandle(hSnap);
+//	return modBaseAddress;
+//}
+
+//uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) {
+//
+//	uintptr_t addr = ptr;
+//	for (unsigned int i = 0; i < offsets.size(); i++) {
+//		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
+//		addr += offsets[i];
+//	}
+//	return addr;
+//}
+
+
+DWORD GetModuleBaseAddress(wchar_t* lpszModuleName, DWORD pID) {
+	DWORD dwModuleBaseAddress = 0;
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID); // make snapshot of all modules within process
+	MODULEENTRY32 ModuleEntry32 = { 0 };
+	ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+
+	if (Module32First(hSnapshot, &ModuleEntry32)) //store first Module in ModuleEntry32
+	{
+		do {
+			if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0) // if Found Module matches Module we look for -> done!
+			{
+				dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+				break;
+			}
+		} while (Module32Next(hSnapshot, &ModuleEntry32)); // go through Module entries in Snapshot and store in ModuleEntry32
+
+
 	}
-	CloseHandle(hSnap);
-	return modBaseAddress;
+	CloseHandle(hSnapshot);
+	return dwModuleBaseAddress;
 }
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) {
+DWORD GetPointerAddress(HWND hwnd, DWORD gameBaseAddr, DWORD address, std::vector<DWORD> offsets)
+{
+	DWORD pID = NULL; // Game process ID
+	GetWindowThreadProcessId(hwnd, &pID);
+	HANDLE phandle = NULL;
+	phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+	if (phandle == INVALID_HANDLE_VALUE || phandle == NULL);
 
-	uintptr_t addr = ptr;
-	for (unsigned int i = 0; i < offsets.size(); i++) {
-		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
-		addr += offsets[i];
+	DWORD offset_null = NULL;
+	ReadProcessMemory(phandle, (LPVOID*)(gameBaseAddr + address), &offset_null, sizeof(offset_null), 0);
+	DWORD pointeraddress = offset_null; // the address we need
+	for (int i = 0; i < offsets.size() - 1; i++) // we dont want to change the last offset value so we do -1
+	{
+		ReadProcessMemory(phandle, (LPVOID*)(pointeraddress + offsets.at(i)), &pointeraddress, sizeof(pointeraddress), 0);
 	}
-	return addr;
+	return pointeraddress += offsets.at(offsets.size() - 1); // adding the last offset
 }
